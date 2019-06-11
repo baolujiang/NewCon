6593d3c26533cbef8de3d7f51b0b5229084a5936
bjiang <bjiang@pharmamedica.com>
2019-06-11 08:57:18 -0400

    fix car.cs ToString() error

---
+++ Console1/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    public class Car
+    {
+
+    	public string Name {get;set;}
+
+    	public void Move()
+    	{
+    		Console.WriteLine("Moving");
+    	}
+
+    	public override string ToString() => Name;
+    }
+}
