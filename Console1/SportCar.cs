46b9816ac2238c6cf40d66874e33be9f73fc977c
bjiang <bjiang@pharmamedica.com>
2019-06-11 09:21:11 -0400

    same program changes

---
+++ Console1/SportCar.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+	public class SportCar:Car
+	{
+
+		public void MakeNoise()=>Console.WriteLine("Hump Hump Hump");
+	}
+}
